e545dd26de77613dc1295fa5ba92fc8cd31e0762
KaranWadhwani <72188110+KaranWadhwani@users.noreply.github.com>
2022-09-06 18:31:51 +0530

    Initial commit

---
+++ assets/js/typing.js
@@ -0,0 +1,11 @@
+
+$(function () {
+    $(".typed").typed({
+        strings: ["Aiming to make a difference", "Quality matters because you matter", "Enlightening World, Enlightening Lives","Achieve beyond your imagination"],
+        typeSpeed: 10,
+        backSpeed: 10,
+        backDelay: 1500,
+        showCursor: false,
+        loop: true
+    });
+});
